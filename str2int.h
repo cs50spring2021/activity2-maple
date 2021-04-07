af71654192ebacb5734c5606032bdd52674c5e6e
Giorgiana C. McCombe <f0044s7@plank.thayer.dartmouth.edu>
2021-04-07 09:56:06 -0400

    added c and h files for str2int func

---
+++ str2int.h
@@ -0,0 +1,9 @@
+/* ***************** str2int ********************** */
+/*                                                                                    
+ * Convert a string to an integer, returning that integer.                            
+ * Returns true if successful, or false if any error.                                 
+ * It is an error if there is any additional character beyond the integer.            
+ * Assumes number is a valid pointer.                                                 
+ */
+bool str2int(const char string[], int * number);
+
